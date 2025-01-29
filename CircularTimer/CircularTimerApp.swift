dd089126fe2b6a5ece648955592fa885111cb4ae
Afeez <harfyzz@gmail.com>
2025-01-29 10:28:28 +0000

    Initial Commit

---
+++ CircularTimer/CircularTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularTimerApp.swift
+//  CircularTimer
+//
+//  Created by Afeez Yunus on 29/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct CircularTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircularTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CircularTimer
+//
+//  Created by Afeez Yunus on 29/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
